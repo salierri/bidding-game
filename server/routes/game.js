2e75cdd0b5317e597929ac2886dfb2da6b9d3f04
Gulyás Gergely <g.gergely314@gmail.com>
2018-06-21 21:38:09 +0200

    API description

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+  route: /new
+  params: -
+  response: { name: 'Name' }
+*/
+
 router.post('/new', function (req, res, next) {
   res.json({ name: 'KIRály csapat 1' });
 });
+
+
+/*
+  route: /bid
+  params: {
+   standings: {
+     coins: {
+       own: 17,
+       other: 183
+     },
+     trophies: {
+       own: 2,
+       other: 4
+     }
+   }
+  }
+  response: { amount: 10 }
+*/
 
 router.post('/bid', function (req, res, next) {
   var maximum = req.body.standings.coins.own;
